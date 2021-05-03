83eccbd7e8e334946c17bbff3c8d70ed7f0ae90f
rturra <rturra@f4.co>
2021-05-03 00:34:14 -0300

    adding to db

--- management-api/src/app.py
+++ management-api/src/app.py
@@ -4,6 +4,8 @@ from sanic_restful_api import reqparse, abort, Api, Resource
 from minio import Minio
 from io import BytesIO
 from databases import Database
+from marshmallow import Schema, fields
+
 
 app = Sanic("sanic-api")
 api = Api(app)
@@ -18,21 +20,23 @@ client = Minio(
     secure=False
 )
 
-
+database = Database('postgresql://postgres:ccom@postgres-db:5432/ccom')
+app.db = database
 products = [
     {'id': '1', 'title': 'product1', 'description': 'joia1', 'price': 'R$5', 'img': 'http://localhost:9000/my-bucket/portfolio_2020_min.png'},
     {'id': '2', 'title': 'product2', 'description': 'joia2', 'price': 'R$100', 'img': 'http://localhost:9000/my-bucket/portfolio_2020_min.png'}
 ]
 
-def setup_database():    
-    app.db = Database('postgresql://postgres:ccom@postgres-db:5432/ccom')    
-    @app.listener('after_server_start')    
-    async def connect_to_db(*args, **kwargs):        
-        await app.db.connect()    
-    @app.listener('after_server_stop')    
-    async def disconnect_from_db(*args, **kwargs):        
-        await app.db.disconnect()
+class ProductSchema(Schema):
+    id = fields.Str()
+    title = fields.Str()
+    description = fields.Str()
 
+
+
+@app.listener('after_server_start')    
+async def connect_to_db(*args, **kwargs):        
+    await app.db.connect()
 
 class Product(Resource):
     async def get(self, request, product_id):
@@ -45,9 +49,13 @@ class Product(Resource):
         pass
 
 class ProductsList(Resource):
-    async def get(self, request):
+    async def get(self, request): 
+        query = f"SELECT * from products"
+        products = await app.db.fetch_all(query=query)
+        schema = ProductSchema(many=True)
+        result = schema.dump(products)
         return response.json(
-        products,
+        result,
         headers={'Content-Range': 'products 0-24/319'},
         status=200)
 
@@ -66,5 +74,5 @@ api.add_resource(Product, '/products/<product_id>')
 
 
 if __name__ == '__main__':
-    setup_database()
+    # setup_database()
     app.run(host="0.0.0.0", port=8000, debug=True)
--- postgres-db/01-dump.sql
+++ postgres-db/01-dump.sql
@@ -1,4 +1,5 @@
 CREATE TABLE public.products (
+    id SERIAL PRIMARY KEY,
     name character varying(255),
     title character varying(255),
     description character varying(255)
